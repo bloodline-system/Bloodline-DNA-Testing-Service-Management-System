d02f41e9860a2fed038c6e48cf0d9c0042a96423
Thuytien4325 <thuyytien4325@gmail.com>
2026-04-16 19:21:09 +0700

    up

--- src/main/java/com/dna_testing_system/dev/repository/NotificationRepository.java
+++ src/main/java/com/dna_testing_system/dev/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.dna_testing_system.dev.repository;
 
 import com.dna_testing_system.dev.entity.Notification;
+import com.dna_testing_system.dev.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
